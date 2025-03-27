3b0e5c1800514ae8b2d7150e0b79cdf47f27f58d
NonaSuomy <nonasuomy@live.com>
2025-03-27 02:08:33 -0400

    Update lock.h

--- components/homekit/lock.h
+++ components/homekit/lock.h
@@ -1,69 +1,72 @@
 #pragma once
-#include <esphome/core/defines.h>
-#ifdef USE_LOCK
-#include <esphome/core/application.h>
-#include <hap.h>
-#include <hap_apple_servs.h>
-#include <hap_apple_chars.h>
-#include <map>
-#ifdef USE_HOMEKEY
-#include <nvs.h>
-#include "const.h"
-#include <HK_HomeKit.h>
-#include <hkAuthContext.h>
-#include <esphome/components/pn532/pn532.h>
-#include <esphome/core/base_automation.h>
-#include "automation.h"
-#endif
 
-namespace esphome
-{
-  namespace homekit
-  {
-    class LockEntity
-    {
-    private:
-      static constexpr const char* TAG = "LockEntity";
-      std::map<AInfo, const char*> accessory_info = {{NAME, NULL}, {MODEL, "HAP-LOCK"}, {SN, NULL}, {MANUFACTURER, "rednblkx"}, {FW_REV, "0.1"}};
-      lock::Lock* ptrToLock;
-      static nvs_handle savedHKdata;
-      static readerData_t readerData;
-      uint8_t tlv8_data[128];
-      #ifdef USE_HOMEKEY
-      std::vector<uint8_t> ecpData{ 0x6A, 0x2, 0xCB, 0x2, 0x6, 0x2, 0x11, 0x0 };
-      static pn532::PN532* nfc_ctx;
-      std::vector<uint8_t> nfcSupportedConfBuffer{ 0x01, 0x01, 0x10, 0x02, 0x01, 0x10 };
-      std::map<HKFinish, std::vector<uint8_t>> hk_color_vals = { {TAN, {0x01, 0x04, 0xce, 0xd5, 0xda, 0x00}}, {GOLD, {0x01, 0x04, 0xaa, 0xd6, 0xec, 0x00}}, {SILVER, {0x01, 0x04, 0xe3, 0xe3, 0xe3, 0x00}}, {BLACK, {0x01, 0x04, 0x00, 0x00, 0x00, 0x00}} };
-      std::unique_ptr<hap_tlv8_val_t> hkFinishTlvData;
-      hap_tlv8_val_t nfcSupportedConf = {
-        .buf = nfcSupportedConfBuffer.data(),
-        .buflen = nfcSupportedConfBuffer.size()
-      };
-      hap_tlv8_val_t management = {
-        .buf = 0,
-        .buflen = 0
-      };
-      std::vector<HKAuthTrigger *> triggers_onhk_;
-      std::vector<HKFailTrigger *> triggers_onhk_fail_;
-      static int nfcAccess_write(hap_write_data_t write_data[], int count, void* serv_priv, void* write_priv);
-      static void hap_event_handler(hap_event_t event, void* data);
-      #endif
-      void on_lock_update(lock::Lock* obj);
-      static int lock_write(hap_write_data_t write_data[], int count, void* serv_priv, void* write_priv);
-      static int acc_identify(hap_acc_t* ha);
+#include "esphome/core/component.h"
+#include "esphome/components/lock/lock.h"
+#include "esp_hap_main.h"
+#include "esp_hap_core.h"
+#include "esp_hap_acc.h"
+#include "esp_hap_serv.h"
+#include "esp_hap_char.h"
 
+namespace esphome {
+namespace homekit {
 
-      public:
-      LockEntity(lock::Lock* lockPtr);
-      void setInfo(std::map<AInfo, const char*> info);
-      void setup();
-      #ifdef USE_HOMEKEY
-      void set_nfc_ctx(pn532::PN532* ctx);
-      void set_hk_hw_finish(HKFinish color);
-      void register_onhk_trigger(HKAuthTrigger* trig);
-      void register_onhkfail_trigger(HKFailTrigger* trig);
-      #endif
+enum class AInfo {
+    NAME,
+    MODEL,
+    SN,
+    MANUFACTURER,
+    FW_REV
+};
+
+enum HKFinish {
+    TAN = 0,
+    GOLD,
+    SILVER,
+    BLACK
+};
+
+class LockEntity {
+private:
+    std::map<AInfo, const char*> accessory_info = {
+        {AInfo::NAME, NULL}, 
+        {AInfo::MODEL, "HAP-LOCK"}, 
+        {AInfo::SN, NULL}, 
+        {AInfo::MANUFACTURER, "rednblkx"}, 
+        {AInfo::FW_REV, "0.1"}
     };
-  }
-}
+    
+    lock::Lock* ptrToLock;
+    std::unique_ptr<hap_tlv8_val_t> hkFinishTlvData;
+
+#ifdef USE_HOMEKEY
+    static nvs_handle savedHKdata;
+    static readerData_t readerData;
+    static pn532::PN532* nfc_ctx;
+    std::vector<uint8_t> ecpData;
+    std::vector<HKAuthTrigger*> triggers_onhk_;
+    std::vector<HKFailTrigger*> triggers_onhk_fail_;
 #endif
+
+public:
+    LockEntity(lock::Lock* lockPtr);
+    void setup();
+    void setInfo(std::map<AInfo, const char*> info);
+    void set_hk_hw_finish(HKFinish color);
+
+protected:
+    static int acc_identify(hap_acc_t* ha);
+    static int lock_write(hap_write_data_t write_data[], int count, void* serv_priv, void* write_priv);
+    void on_lock_update(lock::Lock* obj);
+
+#ifdef USE_HOMEKEY
+    static int nfcAccess_write(hap_write_data_t write_data[], int count, void* serv_priv, void* write_priv);
+    static void hap_event_handler(hap_event_t event, void* data);
+    void register_onhk_trigger(HKAuthTrigger* trig);
+    void register_onhkfail_trigger(HKFailTrigger* trig);
+    void set_nfc_ctx(pn532::PN532* ctx);
+#endif
+};
+
+} // namespace homekit
+} // namespace esphome
